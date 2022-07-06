be8ece10dafcb2a26fc7a9f5c3c1dc567c6462c4
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-07-06 11:32:58 -0300

    Aula 01 react. Refatorando o projeto netflix-clone para um projeto react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import "./netflix.css";
+import Logo from "./assets/images/logo.png";
+import Motivo1 from "./assets/images/motivo-1.png";
+import Motivo2 from "./assets/images/motivo-2.png";
+
+
+/* function App(){
+  const nome = "Wendel";
+  const titulo = "Teste";
+  const idade = 16;
+  return (
+    <div>
+      <header>
+        {titulo}
+        {idade >= 18 ? "Maior" : "Menor"}
+      </header>
+      <main>
+        <h1 classNameName="vermelho">{nome}</h1>
+        <img src={Logo} />
+        <hr />
+      </main>
+    </div>
+  )
+} */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+   <div>
+      <header>
+        <div>
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div id="login">
+          <select id="idioma">
+            <option value="PT">Português</option>
+            <option value="ING">Ingles</option>
+          </select>
+          <button>Entrar</button>
+        </div>
       </header>
-    </div>
+
+      <section id="banner">
+        <h1>
+          Filmes, séries e muito <br />
+          mais. Sem limites.
+        </h1>
+        <h2>Assista onde quiser. Cancele quando quiser.</h2>
+        <p>
+          Pronto para assistir? Informe seu email para criar ou reiniciar sua
+          assinatura.
+        </p>
+        <div>
+          <input type="email" placeholder="Email" />
+          <button>Vamos Lá</button>
+        </div>
+      </section>
+
+      <hr className="divisor" />
+      <section className="motivos">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo1} alt="Motivo 1" />
+        </div>
+      </section>
+
+      <hr className="divisor" />
+      <section className="motivos">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo2} alt="Motivo 2" />
+        </div>
+      </section>
+      <hr className="divisor" />
+
+      <section className="motivos">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo1} alt="Motivo 1" />
+        </div>
+      </section>
+
+      <hr className="divisor" />
+      <section className="motivos alterna">
+        <div>
+          <h1>Aproveite na TV.</h1>
+          <p>
+            Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+            aparelhos de Blu-ray e outros dispositivos.
+          </p>
+        </div>
+        <div>
+          <img src={Motivo1} alt="Motivo 2" />
+        </div>
+      </section>
+      <hr className="divisor" />
+   </div>
   );
 }
 
